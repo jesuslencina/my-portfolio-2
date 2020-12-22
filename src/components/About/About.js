7d51da59fa56053dc1c7956dbcaf9cfc181553b6
Jesús Lencina <jesusignaciolencina@gmail.com>
2020-12-22 15:32:47 -0300

    Backup - About to start working in the About section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,16 +2,15 @@ import React from 'react';
 
 import './AboutCss.css';
 
+import pics from '../../assets/pics.png';
+
 function About() {
   return (
     <section className="section my-section">
       <div class="card my-card">
         <div class="card-image">
           <figure class="image is-16by9">
-            <img
-              src="https://bulma.io/images/placeholders/640x360.png"
-              alt="Picture of me"
-            />
+            <img src={pics} alt="Several pictures of my pets (and one of me)" />
           </figure>
         </div>
       </div>
